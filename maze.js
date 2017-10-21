abb29124e0338a01fbd519e8f4244ea4ba875182
TareekJ <32398435+TareekJ@users.noreply.github.com>
2017-10-20 21:19:24 -0500

    First commit

---
+++ maze.js
@@ -0,0 +1,36 @@
+var loser = null;
+var i= 0;
+
+
+window.onload = function() {
+	document.getElementById("end").onmouseover = endGame;
+	document.getElementById("start").onclick = startGame;
+	var boundary = document.querySelectorAll("div#maze div.boundary");
+	for(i = 0; i < boundary.length; i++) {
+		boundary[i].onmouseover = endGame;
+	}
+};
+
+
+function endGame(event) {
+	if(loser === false) {
+		loser = true;
+		var lose = document.getElementById("status");
+		var boundary = document.querySelectorAll("div#maze div.boundary");
+		for (var i = 0; i < boundary.length; i++) {
+			 boundary[i].classList.add("youlose");
+		}
+	}
+}
+
+
+function startGame() {
+		document.getElementById("status").textContent = "MAKE IT TO THE END";
+		loser = false;
+		var boundary = document.querySelectorAll("div#maze div.boundary");
+		for(i = 0; i < boundary.length; i++) {
+			boundary[i].classList.remove("youlose");
+		}
+}
+
+
